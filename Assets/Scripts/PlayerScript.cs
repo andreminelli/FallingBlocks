b5592b23cfb5cf19850653bdf497ce57375da29e
Andre Minelli <andreminelli@gmail.com>
2018-06-27 18:35:58 -0300

    Base project with player and movement script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    private const int Speed = 7;
+    private float screenHalfWidth;
+    private float playerHalfWidth;
+
+	void Start ()
+    {
+        playerHalfWidth = transform.localScale.x / 2;
+        screenHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
+	}
+	
+	void Update ()
+    {
+        var inputX = Input.GetAxisRaw("Horizontal");
+        var velocity = inputX * Speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        var xMantisse = Math.Abs(transform.position.x);
+        var fullHalfWidth = screenHalfWidth - playerHalfWidth;
+        if (xMantisse > fullHalfWidth)
+        {
+            transform.position = new Vector2(fullHalfWidth * transform.position.x / xMantisse, transform.position.y);
+        }
+    }
+}
