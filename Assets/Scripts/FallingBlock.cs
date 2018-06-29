b01ea7ff0600a284278dbdaa34cd3e50e3e69c70
Andre Minelli <andreminelli@gmail.com>
2018-06-29 19:35:32 -0300

    Destroy falling block when gets out of visible area

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class FallingBlock : MonoBehaviour {
     private const int Speed = 7;
+    private float visibleHeightThreshold;
 
     // Use this for initialization
     void Start () {
-		
+        visibleHeightThreshold = -Camera.main.orthographicSize - transform.localScale.y;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.down * Speed * Time.deltaTime);
+        if (transform.position.y < visibleHeightThreshold)
+        {
+            Destroy(gameObject);
+        }
 	}
 }
