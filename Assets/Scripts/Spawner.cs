b147bb7cb4e9192ebd7cc4872e778e570a6dbccb
Andre Minelli <andreminelli@gmail.com>
2018-07-15 17:49:07 -0300

    Add 2 more asteroid sprites, chosen randomly

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
-	public GameObject FallingBlockPrefab;
+	public GameObject FallingBlockPrefab1;
+	public GameObject FallingBlockPrefab2;
+	public GameObject FallingBlockPrefab3;
+
 	public int Speed = 7;
 	public float SecondsBetweenSpawnsMin = 0.4f;
 	public float SecondsBetweenSpawnsMax = 1;
@@ -13,12 +16,15 @@ public class Spawner : MonoBehaviour {
 	public float SpawnSizeMax = 2f;
 
 
+	GameObject[] fallingBlocksPrefabs;
 	float nextSpawnTime;
 	Vector2 screenHalfWidthWorldUnits;
 
 	// Use this for initialization
 	void Start () {
 		screenHalfWidthWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+
+		fallingBlocksPrefabs = new[] { FallingBlockPrefab1, FallingBlockPrefab2, FallingBlockPrefab3 };
 	}
 
 	// Update is called once per frame
@@ -31,7 +37,8 @@ public class Spawner : MonoBehaviour {
 			var spawnAngle = Random.Range(-SpawnAngleMax, SpawnAngleMax);
 			var spawnSize = Random.Range(SpawnSizeMin, SpawnSizeMax);
 			var spawnPosition = new Vector2(Random.Range(-screenHalfWidthWorldUnits.x, screenHalfWidthWorldUnits.x), screenHalfWidthWorldUnits.y);
-			var newBlock = Instantiate(FallingBlockPrefab, spawnPosition, Quaternion.Euler(Vector3.forward * spawnAngle));
+			var fallingBlockPrefab = fallingBlocksPrefabs[Mathf.RoundToInt(Random.Range(0, fallingBlocksPrefabs.Length-1))];
+			var newBlock = Instantiate(fallingBlockPrefab, spawnPosition, Quaternion.Euler(Vector3.forward * spawnAngle));
 			newBlock.transform.localScale = Vector2.one * spawnSize;
 		}
 
