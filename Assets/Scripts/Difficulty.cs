ec7786049ae162d56dd6449e2976b56e88a32f4e
Andre Minelli <andreminelli@gmail.com>
2018-07-15 17:35:17 -0300

    Increase difficulty over time; add an angle to falling blocks

---
+++ Assets/Scripts/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Difficulty : MonoBehaviour {
+
+    static float SecondsToMaxDifficulty = 20f;
+
+    public static float GetDifficultyPercent()
+        => Mathf.Clamp01(Time.timeSinceLevelLoad / SecondsToMaxDifficulty);
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,7 +6,8 @@ public class Spawner : MonoBehaviour {
 
 	public GameObject FallingBlockPrefab;
 	public int Speed = 7;
-	public float SecondsBetweenSpawns = 1;
+	public float SecondsBetweenSpawnsMin = 0.4f;
+	public float SecondsBetweenSpawnsMax = 1;
 	public float SpawnAngleMax = 15;
 	public float SpawnSizeMin = 0.2f;
 	public float SpawnSizeMax = 2f;
@@ -24,11 +25,13 @@ public class Spawner : MonoBehaviour {
 	void Update () {
 		if (Time.time > nextSpawnTime)
 		{
-			nextSpawnTime = Time.time + SecondsBetweenSpawns;
+			var secondsBetweenSpawns = Mathf.Lerp(SecondsBetweenSpawnsMax, SecondsBetweenSpawnsMin, Difficulty.GetDifficultyPercent());
+			nextSpawnTime = Time.time + secondsBetweenSpawns;
+
 			var spawnAngle = Random.Range(-SpawnAngleMax, SpawnAngleMax);
 			var spawnSize = Random.Range(SpawnSizeMin, SpawnSizeMax);
 			var spawnPosition = new Vector2(Random.Range(-screenHalfWidthWorldUnits.x, screenHalfWidthWorldUnits.x), screenHalfWidthWorldUnits.y);
-			var newBlock = (GameObject)Instantiate(FallingBlockPrefab, spawnPosition, Quaternion.identity);
+			var newBlock = Instantiate(FallingBlockPrefab, spawnPosition, Quaternion.Euler(Vector3.forward * spawnAngle));
 			newBlock.transform.localScale = Vector2.one * spawnSize;
 		}
 
