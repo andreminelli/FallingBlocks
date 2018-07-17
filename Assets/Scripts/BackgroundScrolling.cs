c914bb29bd76c30325821e6db5d5128c1f81ab7a
Andre Minelli <andreminelli@gmail.com>
2018-07-17 16:19:05 -0300

    Add sprit background, with scrolling

---
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour {
+
+	public float Velocity = 0.3f;
+	Material material;
+
+	// Use this for initialization
+	void Start () {
+		material = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		material.mainTextureOffset = new Vector2(0, Velocity) * Time.timeSinceLevelLoad;
+	}
+}
